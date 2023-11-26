8cc6a940b41ef8d13eb2bded4030b72026d39be1
c483481 <charleskojansow@gmail.com>
2023-11-27 01:56:46 +0800

    feat: add get list cell endpoint

--- src/server/controller/cell.controller.ts
+++ src/server/controller/cell.controller.ts
@@ -7,6 +7,7 @@ import { CellValidator } from "../validate/cell.validator";
 import { defaultMiddleware } from "../../utils/middleware-helper.utils";
 import { Privilege } from "../../constant/privilege.constant";
 import { WrapAppHandler } from "../../handler/default.handler";
+import { getListOption } from "../../utils/helper.utils";
 
 export class CellController extends BaseController {
     private service!: CellService;
@@ -18,6 +19,8 @@ export class CellController extends BaseController {
     }
     initRoute(): void {
         this.router.post("/", defaultMiddleware(Privilege.penjaga), WrapAppHandler(this.postCreateCell));
+
+        this.router.get("/", defaultMiddleware(), WrapAppHandler(this.getListCell));
     }
 
     postCreateCell = async (req: Request): Promise<unknown> => {
@@ -26,6 +29,14 @@ export class CellController extends BaseController {
         validate(CellValidator.CellCreation_Payload, payload);
 
         const result = await this.service.createCell(payload);
+
+        return result;
+    };
+
+    getListCell = async (req: Request): Promise<unknown> => {
+        const payload = getListOption(req);
+
+        const result = await this.service.getListCell(payload);
 
         return result;
     };
