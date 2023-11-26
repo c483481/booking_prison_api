a73690388b97acea51d9eeb541b4aec52286c875
c483481 <charleskojansow@gmail.com>
2023-11-27 04:12:32 +0800

    feat: add get list napi endpoint

--- src/server/controller/napi.controller.ts
+++ src/server/controller/napi.controller.ts
@@ -7,6 +7,7 @@ import { NapiValidator } from "../validate/napi.validator";
 import { defaultMiddleware } from "../../utils/middleware-helper.utils";
 import { Privilege } from "../../constant/privilege.constant";
 import { WrapAppHandler } from "../../handler/default.handler";
+import { getListOption } from "../../utils/helper.utils";
 
 export class NapiController extends BaseController {
     private service!: NapiService;
@@ -21,6 +22,8 @@ export class NapiController extends BaseController {
 
     initRoute(): void {
         this.router.post("/", defaultMiddleware(Privilege.penjaga), WrapAppHandler(this.postCreateNapi));
+
+        this.router.get("/", defaultMiddleware(), WrapAppHandler(this.getListNapi));
     }
 
     postCreateNapi = async (req: Request): Promise<unknown> => {
@@ -29,6 +32,14 @@ export class NapiController extends BaseController {
         validate(NapiValidator.NapiCreation_Payload, payload);
 
         const result = await this.service.createNapi(payload);
+
+        return result;
+    };
+
+    getListNapi = async (req: Request): Promise<unknown> => {
+        const payload = getListOption(req);
+
+        const result = await this.service.listNapi(payload);
 
         return result;
     };
